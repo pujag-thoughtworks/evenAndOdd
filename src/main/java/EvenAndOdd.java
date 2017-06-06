9fad6c3f37e720c01a3539a0a7151c0bd43105ec
Puja Gupta <pujag@thoughtworks.com>
2017-06-06 14:54:32 +0530

    First commit

---
+++ src/main/java/EvenAndOdd.java
@@ -0,0 +1,13 @@
+/**
+ * Created by pujag on 6/6/17.
+ */
+public class EvenAndOdd {
+    int num;
+    EvenAndOdd(int n) {
+        num=n;
+    }
+    public boolean isEven() {
+        return(num%2==0);
+    }
+}
+
---
+++ src/test/java/EvenOddTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by pujag on 6/6/17.
+ */
+public class EvenOddTest {
+    @Test
+    public void twoshouldBeEven() {
+        EvenAndOdd evn=new EvenAndOdd(2);
+        assertTrue(evn.isEven());
+    }
+}
